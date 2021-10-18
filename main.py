ec0bcf2ddba54a80046ba54f2b31883802a171bb
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-10-17 23:02:32 -0400

    fetching html

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import time
+import requests
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pprint
 
 fb = "hassanhanjra900@gmail.com"
 pw = 9018201778
 
 driver = webdriver.Chrome('./chromedriver', keep_alive=True)
-driver.get("https://artists.spotify.com/")
+driver.get("https://accounts.spotify.com/en/login?continue=https:%2F%2Fartists.spotify.com%2F")
 time.sleep(4)
 
-driver.find_element_by_xpath("/html/body/div[1]/div/div/div/header/div[2]/ul/li[1]/a").click()
+# driver.find_element_by_xpath("/html/body/div[1]/div/div/div/header/div[2]/ul/li[1]/a").click()
 time.sleep(4)
 
 driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[1]/div/a").click()
@@ -37,12 +39,22 @@ playlist.click()
 
 
 time.sleep(4)
-# downloadsvg = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/main/div/div/div/div[2]/div[2]/div/div[2]/div/div/span/span/a/svg/line")
-downloadcsv = driver.get("https://artistinsights-downloads.spotify.com/v1/artist/7KzG8dszzwlSDGEsCbzANz/downloads/playlists.csv?time-filter=28day")
 
-time.sleep(10)
+# working download
+# downloadcsv = driver.get("https://artistinsights-downloads.spotify.com/v1/artist/7KzG8dszzwlSDGEsCbzANz/downloads/playlists.csv?time-filter=28day")
 
-drive.quit()
+res = requests.get("https://artists.spotify.com/c/artist/7KzG8dszzwlSDGEsCbzANz/music/playlists")
+print(res.status_code)
+a  = str(res.content)
+
+with open("test.html","w") as fp:
+    fp.write(a)
+    print("saved")
+# pprint.pprint(res)
+
+time.sleep(5)
+
+driver.quit()
 
 
 # add beautifulsoup to extract the playlist data and save it in a file.
