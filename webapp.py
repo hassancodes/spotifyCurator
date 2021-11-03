4657548c36b8eb22ba1cfd9f52556e055555479d
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-11-03 14:47:07 -0400

    fix: html and css connection

--- webapp.py
+++ webapp.py
@@ -1,12 +1,14 @@
-from flask import Flask
+import flask
+from flask import Flask,escape
 from flask import render_template
 from flask import send_file
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return send_file("static\HTML\index.html")
+    return send_file("templates/index.html")
 
 @app.route("/main")
 def main():
@@ -14,4 +16,4 @@ def main():
 
 
 
-app.run("127.0.0.1",port=8080)
+app.run("127.0.0.1",port=8080 ,debug=True)
