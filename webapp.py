ec0cf3ea0d05fced9b15e2ec5c03ccb04dc12756
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-11-03 15:36:46 -0400

    updating webapp

--- webapp.py
+++ webapp.py
@@ -1,14 +1,22 @@
 import flask
+import json
 from flask import Flask,escape
 from flask import render_template
 from flask import send_file
 
 app = Flask(__name__)
 
-
+file = open("dump.json", "r")
+a = json.load(file)
+print(a)
 @app.route("/")
 def hello_world():
-    return send_file("templates/index.html")
+    # return send_file("templates/index.html")
+    return f"<p>Hello {json.loads(a)}</p>"
+
+
+
+
 
 @app.route("/main")
 def main():
